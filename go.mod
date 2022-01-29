8102566d30cfa62af86c8f8658c5b501cca1549f
joshin@dal.ca <joshin@dal.ca>
2022-01-29 12:38:18 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joshinjohnson/authentication-manager
+
+go 1.16
+
+require github.com/joshinjohnson/authentication-engine v0.1.0 // indirect
---
+++ internal/operation/manager.go
@@ -0,0 +1,11 @@
+package operation
+
+import "github.com/joshinjohnson/authentication-engine/pkg/api"
+
+type AuthenticationManager struct {
+    engine api.AuthenticationEngine
+}
+
+func NewAuthenticationManager() *AuthenticationManager {
+    return nil
+}
